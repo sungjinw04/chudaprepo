75519ab4cbecbd361e53d60841159cff79afed06
Aditya Tripathi <abhishekpandit4224@gmail.com>
2024-11-04 19:16:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://eldiananimates:sung@clust
 DB_NAME = os.environ.get("DATABASE_NAME", "eldiananimates")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "eldian_networks"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", 'eldian_networks'))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
